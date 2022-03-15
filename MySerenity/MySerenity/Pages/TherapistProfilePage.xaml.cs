bfaf8361e6c13373fcb45cc9068779eaf1f2301b
Daniel Nicholson <Danny.nicholson95@gmail.com>
2022-03-15 16:49:00 +0000

    Adding profile page for therapist - commiting to master before making new branch to implement to functionality.

---
+++ MySerenity/MySerenity/Pages/TherapistProfilePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MySerenity.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TherapistProfilePage : ContentPage
+    {
+        public TherapistProfilePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
